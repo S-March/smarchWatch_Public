12b4ad373873ced0ac3a157dc07d13784deb1409
S-March <samsonleomarch@gmail.com>
2019-05-03 14:17:22 -0500

    Removed SDK files at request of Dialog Semiconductor
    Non-SDK files remain. Will add link to download SDK from Dialogs website (it is free)

--- Software/smarchWatch_DA14683/DA1468x_SDK_1.0.14.1081/DA1468x_DA15xxx_SDK_1.0.14.1081/sdk/bsp/include/black_orca.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BLACK_ORCA_H__
-#define __BLACK_ORCA_H__
-
-#pragma message "This file is obsolete. Please use sdk_defs.h instead!"
-#include "sdk_defs.h"
-
-#endif  /* __BLACK_ORCA_H__ */
-
--- Software/smarchWatch_DA14683/DA1468x_SDK_1.0.14.1081/DA1468x_DA15xxx_SDK_1.0.14.1081/sdk/bsp/include/global_io.h
+++
@@ -1,8 +0,0 @@
-#ifndef IO_H_INCLUDED
-#define IO_H_INCLUDED
-
-#pragma message "This file is obsolete. Please use sdk_defs.h instead!"
-#include "sdk_defs.h"
-
-#endif
-
